3d06705c325084cf0ab7d40572207a6986800dd0
Polash Mahmud <polashmahmud@gmail.com>
2024-02-18 18:14:14 +0600

    add ignore dead links

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "vitepress";
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  ignoreDeadLinks: true,
   title: "পিএইচপি বেসিক কোর্স",
   description:
     "একটি বিগেনার ফ্রেন্ডলি পিএইচপি কোর্স | আমরা যারা একদম নতুন তাদের সহজ ভাষায় পিএইচপি শিখানোর চেষ্টা করি ।",
