a100572b1196aac29806dc9e05e9df42f73a6e13
Polash Mahmud <polashmahmud@gmail.com>
2024-05-11 20:31:43 +0600

    change sidebar

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -1,101 +1,88 @@
-import { defineConfig } from "vitepress";
+import {defineConfig} from "vitepress";
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  ignoreDeadLinks: true,
-  title: "পিএইচপি বেসিক কোর্স",
-  description:
-    "একটি বিগেনার ফ্রেন্ডলি পিএইচপি কোর্স | আমরা যারা একদম নতুন তাদের সহজ ভাষায় পিএইচপি শিখানোর চেষ্টা করি ।",
-  themeConfig: {
-    // https://vitepress.dev/reference/default-theme-config
-    nav: [
-      { text: "হোম পেজ", link: "/" },
-      { text: "ডকুমেন্টেশন", link: "/getting-started/what-is-php" },
-      { text: "টিম", link: "/team" },
-    ],
-
-    sidebar: [
-      {
-        text: 'শুরু করার উপায়',
-        collapsed: false,
-        items: [
-          { text: 'PHP এর ইতিহাস', link: '/getting-started/what-is-php' },
-          { text: 'PHP ইনস্টলেশন', link: '/getting-started/install-php' },
-          { text: 'PHP Tags', link: '/getting-started/php-tags' },
-        ]
-      },
-      {
-        text: 'পিএইচপি মৌলিক',
-        collapsed: true,
-        items: [
-          { text: 'Variables', link: '/php-fundamentals/variables' },
-          { text: 'Type of data in PHP', link: '/php-fundamentals/types-of-data-in-php' },
-          { text: 'Constant', link: '/php-fundamentals/constant' },
-          { text: 'Comments', link: '/php-fundamentals/comments' },
-        ]
-      },
-      {
-        text: "ক্লাস সমূহ",
-        items: [
-          { text: "দ্বিতীয় ক্লাস", link: "/class-02" },
-          { text: "তৃতীয় ক্লাস", link: "/class-03" },
-          { text: "চতুর্থ ক্লাস", link: "/class-04" },
-          { text: "পঞ্চম ক্লাস", link: "/class-05" },
-          { text: "ষষ্ঠ ক্লাস", link: "/class-06" },
-          { text: "সপ্তম ক্লাস", link: "/class-07" },
-          { text: "অষ্টম ক্লাস", link: "/class-08" },
-          { text: "নবম ক্লাস", link: "/class-09" },
-          { text: "দশম ক্লাস", link: "/class-10" },
-          { text: "একাদশ ক্লাস", link: "/class-11" },
-          { text: "দ্বাদশ ক্লাস", link: "/class-12" },
+    ignoreDeadLinks: true,
+    title: "পিএইচপি বাংলা ডকুমেন্টেশন",
+    description:
+        "একটি বিগেনার ফ্রেন্ডলি পিএইচপি কোর্স | আমরা যারা একদম নতুন তাদের সহজ ভাষায় পিএইচপি শিখানোর চেষ্টা করি ।",
+    themeConfig: {
+        // https://vitepress.dev/reference/default-theme-config
+        nav: [
+            {text: "হোম পেজ", link: "/"},
+            {text: "ডকুমেন্টেশন", link: "/getting-started/what-is-php"},
+            {text: "টিম", link: "/team"},
         ],
-      },
-    ],
 
-    socialLinks: [
-      { icon: "github", link: "https://github.com/polashmahmud/php" },
-      {
-        icon: "facebook",
-        link: "https://www.facebook.com/learnwithpolashmahmud",
-      },
-      { icon: "youtube", link: "https://www.youtube.com/polashmahmud4" },
-      { icon: "linkedin", link: "https://www.linkedin.com/in/polashmahmud4/" },
-      { icon: "discord", link: "https://discord.gg/mktPP7n9xp" },
-      { icon: "x", link: "https://twitter.com/polashmahmud4" },
-    ],
+        sidebar: [
+            {
+                items: [
+                    {text: 'পিএইচপি এর ইতিহাস', link: '/getting-started/what-is-php'},
+                    {text: 'পিএইচপি ইনস্টলেশন', link: '/getting-started/install-php'},
+                    {text: 'পিএইচপি ট্যাগ', link: '/getting-started/php-tags'},
+                    {text: 'পিএইচপি ভেরিয়েবল', link: '/php-fundamentals/variables'},
+                    {text: 'পিএইচপি ডেটা টাইপ', link: '/php-fundamentals/types-of-data-in-php'},
+                    {text: 'পিএইচপি কনস্ট্যান্ট', link: '/php-fundamentals/constant'},
+                    {text: 'পিএইচপি কমেন্টস', link: '/php-fundamentals/comments'},
+                    {text: "পিএইচপি অপারেশন এবং এক্সপ্রেশন", link: "/class-02"},
+                    {text: "পিএইচপি বেসিক", link: "/class-03"},
+                    {text: "পিএইচপি ফাংশনাল প্রোগ্রামিং এবং Scope পরিচিতি", link: "/class-04"},
+                    {text: "পিএইচপি Array পরিচিতি ও অপারেশন", link: "/class-05"},
+                    {text: "পিএইচপি Array ভিত্তিক প্রয়োজনীয় কিছু ফাংশন", link: "/class-06"},
+                    {text: "পিএইচপির সাহায্যে এইচটিএমএল ফর্ম ম্যানিপুলেশন", link: "/class-07"},
+                    {text: "পিএইচপি দিয়ে HTML ফর্ম ডাটার Advance ম্যানিপুলেশন", link: "/class-08"},
+                    {text: "পিএইচপি দিয়ে HTML ফর্ম ডাটার Advance ম্যানিপুলেশন", link: "/class-09"},
+                    {text: "পিএইচপি প্রজেক্টঃ 01 - Build Image Gallery", link: "/class-10"},
+                    {text: "পিএইচপি তারিখ ও সময় ফরম্যাটিং", link: "/class-11"},
+                    {text: "PHP DateTime ফরম্যাটিংয়ে Carbon এর ব্যবহার", link: "/class-12"},
+                ]
+            },
+        ],
 
-    footer: {
-      message: "Released under the MIT License.",
-      copyright: "Copyright © 2023-present Polash Mahmud",
+        socialLinks: [
+            {icon: "github", link: "https://github.com/polashmahmud/php"},
+            {
+                icon: "facebook",
+                link: "https://www.facebook.com/learnwithpolashmahmud",
+            },
+            {icon: "youtube", link: "https://www.youtube.com/polashmahmud4"},
+            {icon: "linkedin", link: "https://www.linkedin.com/in/polashmahmud4/"},
+            {icon: "discord", link: "https://discord.gg/mktPP7n9xp"},
+            {icon: "x", link: "https://twitter.com/polashmahmud4"},
+        ],
+
+        footer: {
+            message: "Released under the MIT License.",
+            copyright: "Copyright © 2023-present Polash Mahmud",
+        },
+
+        lastUpdated: {
+            text: "Updated at",
+            formatOptions: {
+                dateStyle: "full",
+                timeStyle: "medium",
+            },
+        },
+
+        search: {
+            provider: "local",
+        },
     },
-
-    lastUpdated: {
-      text: "Updated at",
-      formatOptions: {
-        dateStyle: "full",
-        timeStyle: "medium",
-      },
-    },
-
-    search: {
-      provider: "local",
-    },
-  },
-  head: [
-    [
-      "script",
-      {
-        async: "",
-        src: "https://www.googletagmanager.com/gtag/js?id=G-7RDGBXJVRS",
-      },
-    ],
-    [
-      "script",
-      {},
-      `window.dataLayer = window.dataLayer || [];
+    head: [
+        [
+            "script",
+            {
+                async: "",
+                src: "https://www.googletagmanager.com/gtag/js?id=G-7RDGBXJVRS",
+            },
+        ],
+        [
+            "script",
+            {},
+            `window.dataLayer = window.dataLayer || [];
       function gtag(){dataLayer.push(arguments);}
       gtag('js', new Date());
       gtag('config', 'G-7RDGBXJVRS');`,
+        ],
     ],
-  ],
 });
